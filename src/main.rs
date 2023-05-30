20c50abf2711cf6a09781f45badafb58581e35d9
Robert Reinert <robert.reinert@gmail.com>
2023-05-30 18:37:52 +0000

    added pg_query function

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
 extern crate csv;
 
 use sqlx::postgres::PgPoolOptions;
-// use std::error::Error;
+
+use std::error::Error;
 use std::io;
 // use std::process;
 
+async fn pg_query(
+    query: String,
+    pool: sqlx::Pool<sqlx::Postgres>,
+) -> Result<sqlx::postgres::PgQueryResult, Box<(dyn Error + 'static)>> {
+    let result: sqlx::postgres::PgQueryResult = sqlx::query(&query).execute(&pool).await?;
+    Ok(result)
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
-    let pool = PgPoolOptions::new()
+async fn main() -> Result<(), Box<(dyn Error + 'static)>> {
+    let pool: sqlx::Pool<sqlx::Postgres> = PgPoolOptions::new()
     .max_connections(5)
     .connect("postgres://filesusqladmin:ujp@wdr3KMW7zpr!rmh@bccrc-pr-cc-fu-psql.postgres.database.azure.com/universe").await?;
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    let mut query = "".to_owned();
-    let mut count = 0;
+    let mut rdr: csv::Reader<io::Stdin> = csv::Reader::from_reader(io::stdin());
+    let mut query: String = "INSERT INTO files (
+        url,
+        name,
+        account,
+        container,
+        resourcetype,
+        createdon,
+        lastmodified,
+        contentlength,
+        contentmd5,
+        accesstier,
+        eventtype
+    )
+    VALUES "
+        .to_owned();
+    let mut count: i32 = 0;
     for result in rdr.records() {
-        let record = result?;
-        let account = "bccrcdmgshahlabdb06sa03";
-        let eventtype = "Microsoft.Storage.BlobCreated";
+        let record: csv::StringRecord = result?;
+        let account: &str = "bccrcdmgshahlabdb06sa03";
+        let eventtype: &str = "Microsoft.Storage.BlobCreated";
         let name_vec: Vec<&str> = record[0].split('/').collect();
-        let url = format!("https://{}.blob.core.windows.net/{}", account, &record[0]);
-        let subquery = format!(
-            "INSERT INTO files (
-                url,
-                name,
-                account,
-                container,
-                resourcetype,
-                createdon,
-                lastmodified,
-                contentlength,
-                contentmd5,
-                accesstier,
-                eventtype
-            )
-            VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')
-            ON CONFLICT (url) DO NOTHING
-            ",
+        let url: String = format!("https://{}.blob.core.windows.net/{}", account, &record[0]);
+        let subquery: String = format!(
+            "('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')",
             url,
             &record[0],
             account,
@@ -50,29 +58,38 @@ async fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
         );
         // println!("{}", query);
         query.push_str(&subquery);
-        if count == 100 {
-            let _result = sqlx::query(&query).execute(&pool).await?;
+
+        if count == 200 {
+            query.push_str(
+                "
+            ON CONFLICT (url) DO NOTHING",
+            );
+            let _result: sqlx::postgres::PgQueryResult = sqlx::query(&query).execute(&pool).await?;
             // println!("{:?}", query);
-            query = "".to_owned();
+            query = "INSERT INTO files (
+                url,
+                name,
+                account,
+                container,
+                resourcetype,
+                createdon,
+                lastmodified,
+                contentlength,
+                contentmd5,
+                accesstier,
+                eventtype
+            )
+            VALUES "
+                .to_owned();
             count = 0;
             continue;
+            // break;
         }
+        query.push_str(
+            ",
+        ",
+        );
         count += 1;
     }
     Ok(())
-    // if let Err(err) = run() {
-    //     println!("{}", err);
-    //     process::exit(1);
-    // }
 }
-
-// fn run() -> Result<(), Box<dyn Error>> {
-//     let mut rdr = csv::Reader::from_reader(io::stdin());
-//     for result in rdr.records() {
-//         // This is effectively the same code as our `match` in the
-//         // previous example. In other words, `?` is syntactic sugar.
-//         let record = result?;
-//         println!("{:?}", record);
-//     }
-//     Ok(())
-// }
