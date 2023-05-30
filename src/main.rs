99e77d3880f7bf0b10ec047804960e749587824d
Robert Reinert <robert.reinert@gmail.com>
2023-05-30 20:24:09 +0000

    Added dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
 extern crate csv;
+extern crate dotenv;
 
+use dotenv::dotenv;
 use sqlx::postgres::PgPoolOptions;
+use std::env;
 
+use clap::Parser;
 use std::error::Error;
 use std::io;
-// use std::process;
+
+/// Imports a CSV file into the Files Universe database
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// CSV file to read
+    #[arg(short, long)]
+    input: String,
+
+    /// Storage account name of the CSV file
+    #[arg(short, long)]
+    account: String,
+}
 
 async fn pg_query(
-    query: String,
-    pool: sqlx::Pool<sqlx::Postgres>,
+    query: &String,
+    pool: &sqlx::Pool<sqlx::Postgres>,
 ) -> Result<sqlx::postgres::PgQueryResult, Box<(dyn Error + 'static)>> {
-    let result: sqlx::postgres::PgQueryResult = sqlx::query(&query).execute(&pool).await?;
+    let result: sqlx::postgres::PgQueryResult = sqlx::query(query).execute(pool).await?;
     Ok(result)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<(dyn Error + 'static)>> {
+    dotenv().ok();
+    let args = Args::parse();
     let pool: sqlx::Pool<sqlx::Postgres> = PgPoolOptions::new()
     .max_connections(5)
     .connect("postgres://filesusqladmin:ujp@wdr3KMW7zpr!rmh@bccrc-pr-cc-fu-psql.postgres.database.azure.com/universe").await?;
@@ -36,6 +54,7 @@ async fn main() -> Result<(), Box<(dyn Error + 'static)>> {
     VALUES "
         .to_owned();
     let mut count: i32 = 0;
+    let mut queries: Vec<String> = Vec::new();
     for result in rdr.records() {
         let record: csv::StringRecord = result?;
         let account: &str = "bccrcdmgshahlabdb06sa03";
@@ -59,13 +78,22 @@ async fn main() -> Result<(), Box<(dyn Error + 'static)>> {
         // println!("{}", query);
         query.push_str(&subquery);
 
-        if count == 200 {
+        if count == 100 {
             query.push_str(
                 "
             ON CONFLICT (url) DO NOTHING",
             );
-            let _result: sqlx::postgres::PgQueryResult = sqlx::query(&query).execute(&pool).await?;
+            let push_query = query.clone();
+            queries.push(push_query);
             // println!("{:?}", query);
+            if queries.len() == 3 {
+                let (_first, _second, _third) = tokio::join!(
+                    pg_query(&queries[0], &pool),
+                    pg_query(&queries[1], &pool),
+                    pg_query(&queries[2], &pool)
+                );
+                queries.clear();
+            }
             query = "INSERT INTO files (
                 url,
                 name,
